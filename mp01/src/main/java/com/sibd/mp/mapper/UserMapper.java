232dd50bb335a92d82f103f7bf3dd69204ec5ee5
hai <565150280@qq.com>
2022-01-06 09:30:09 +0800

    first commit

--- mp01/src/main/java/com/sibd/mp/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sibd.mp.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sibd.mp.pojo.User;
-import org.apache.ibatis.annotations.Select;
-
-public interface UserMapper extends BaseMapper<User> {
-    User findById(Long id);
-}
